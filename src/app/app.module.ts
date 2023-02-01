6b4a4018c06caf5ee041f404f4fb59ff1d3e1709
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-01-31 23:23:59 -0300

    Renderização de listas
    - *ngFor="let item of items"
    - ngFor de obj dogs(name, type)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { IfRenderComponent } from './components/if-render/if-render.component';
 import { EventsComponent } from './components/events/events.component';
 import { EmitterComponent } from './components/emitter/emitter.component';
 import { ChangeNumberComponent } from './components/change-number/change-number.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { ChangeNumberComponent } from './components/change-number/change-number.
     IfRenderComponent,
     EventsComponent,
     EmitterComponent,
-    ChangeNumberComponent
+    ChangeNumberComponent,
+    ListRenderComponent
   ],
   imports: [
     BrowserModule
