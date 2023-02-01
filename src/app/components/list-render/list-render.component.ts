051a286a001c303dc505a00cb86c3d9fff368ce4
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-02-01 01:12:48 -0300

    Services do Angular
    - requisições para APIs
    - ng generate service <nome>
    - importar no component e iniciar no constructor
    - depois acessar os métodos do service

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { Dog } from 'src/app/Dog';
 
+import { ListService } from 'src/app/services/list.service';
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
@@ -16,8 +18,15 @@ export class ListRenderComponent {
 
   dogDetails = '';
 
+  constructor(private listService: ListService) { }
+
   showAge(dog: Dog): void {
     this.dogDetails = `O pet ${dog.name} tem ${dog.age} anos!`;
+  }
+
+  removeDog(dog: Dog): void {
+    console.log('Removendo cachorro...');
+    this.dogs = this.listService.remove(this.dogs, dog);
   }
 
 }
