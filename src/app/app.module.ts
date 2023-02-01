ac92dddfb39c07d26dd38ab7e2f39ad703f758e5
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-02-01 00:31:56 -0300

    Pipe operators do Angular
    - uppercase
    - titlecase
    - percent
    - date: "fullDate"
    - currency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { EventsComponent } from './components/events/events.component';
 import { EmitterComponent } from './components/emitter/emitter.component';
 import { ChangeNumberComponent } from './components/change-number/change-number.component';
 import { ListRenderComponent } from './components/list-render/list-render.component';
+import { PipesComponent } from './components/pipes/pipes.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { ListRenderComponent } from './components/list-render/list-render.compon
     EventsComponent,
     EmitterComponent,
     ChangeNumberComponent,
-    ListRenderComponent
+    ListRenderComponent,
+    PipesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/pipes/pipes.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pipes',
+  templateUrl: './pipes.component.html',
+  styleUrls: ['./pipes.component.css']
+})
+export class PipesComponent {
+  someText = "Testando o pipe operator";
+
+  today = new Date();
+
+}
