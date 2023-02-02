d55f1da27e80692ddd216af0a8ba663e8833be28
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-02-02 01:39:09 -0300

    ngSubmit - Submit Form
    - dentro de form, (ngSubmit)="handleSubmit()"
    - button [disabled]="form.invalid"
    - método handleSubmit no controller para fazer post

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
@@ -12,5 +13,11 @@ export class FormsComponent {
     { lanche: "X-Bacon", preco: "R$12,00" },
     { lanche: "Coxinha", preco: "R$8,00" }
   ]
+
+  submitForm(form: NgForm) {
+    if (form.valid) {
+      console.log(form.value);
+    }
+  }
 
 }
